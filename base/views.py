e22a3d2c9858d83034039b0f57f33593d893484c
Paul Olkov <OlkovPaul@gmail.com>
2023-11-22 10:04:20 +0300

    include the database

--- base/views.py
+++ base/views.py
@@ -1,22 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
-rooms = [
-    {'id': 1, 'name': 'Let\'s learn Python'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend developers'},
-]
+from .models import Room
 
 
+# I stopped at 1:21:30
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == pk:
-            room = i
+    room = get_object_or_404(Room, pk=pk)
+
     context = {"room": room}
     return render(request, 'base/room.html', context)
