5a72404102e136c00bb50bc3717269819bef6a4b
Paul Olkov <OlkovPaul@gmail.com>
2023-11-22 12:23:19 +0300

    ability to add, delete and update posts in front end

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
 
 
 # I stopped at 1:21:30
@@ -15,3 +16,37 @@ def room(request, pk):
 
     context = {"room": room}
     return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+
+    return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = get_object_or_404(Room, id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'request': request, 'obj': room})
